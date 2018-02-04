4024ac7111846c132bc66de35de0a54f4ebefb4b
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-02-04 12:11:18 -0500

    another attempt to make work on heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@
 
 var express = require('express')
 const path = require('path')
-const history = require('connect-history-api-fallback')
+
 
 // Create our app
 
@@ -33,6 +33,10 @@ app.use(function(req, res, next){
 });
 
 app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, function(){
 	console.log("Express server is up on port" + PORT);
