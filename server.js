04ae5176601ace52055c0ce8389d6687fbc29ee7
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-02-04 12:07:51 -0500

    another attempt to make work on heroku

--- server.js
+++ server.js
@@ -1,14 +1,57 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, 'public');
-
-app.use(express.static(publicPath));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(publicPath, 'index.html'));
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const publicPath = path.join(__dirname, 'public');
+//
+// app.use(express.static(publicPath));
+//
+// // app.get('*', (req, res) => {
+// //   res.sendFile(path.join(publicPath, 'index.html'));
+// // });
+//
+// app.listen(3000, () => {
+//   console.log('Server is up!');
 // });
 
-app.listen(3000, () => {
-  console.log('Server is up!');
+var express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+
+// Create our app
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(function(req, res, next){
+	if (req.headers['x-forwarded-proto'] === 'https'){
+		res.redirect('http://' + req.hostname + req.url);
+	}
+	else {
+		next();
+	}
 });
+
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+	console.log("Express server is up on port" + PORT);
+});
+
+
+//
+// const app = express()
+// const staticFileMiddleware = express.static(path.join(__dirname, 'public'))
+//
+// app.use(staticFileMiddleware)
+// app.use(history())
+// app.use(staticFileMiddleware)
+// // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+//
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/index.html'))
+// })
+//
+// app.listen(3000, function () {
+//   console.log( 'Express serving on 3000!' )
+// })
