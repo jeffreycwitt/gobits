c29fbaf02e24aa3372f9b4fbe2ef0cdd47c7ed95
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2020-03-28 10:27:30 -0400

    Updates

---
+++ precache-manifest.9cc54f5f0a6d4e7e08522c25d2cb7921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9dd9d9fa00c1ae762aee18f7681895",
+    "url": "/gobits/index.html"
+  },
+  {
+    "revision": "1c571c94b14674c8267c",
+    "url": "/gobits/static/css/main.a6a88db8.chunk.css"
+  },
+  {
+    "revision": "d5c6c511b861c30466f7",
+    "url": "/gobits/static/js/2.c2a71a6c.chunk.js"
+  },
+  {
+    "revision": "3af990ad73ee07bebb244e4e227f5b3c",
+    "url": "/gobits/static/js/2.c2a71a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c571c94b14674c8267c",
+    "url": "/gobits/static/js/main.ea35eb32.chunk.js"
+  },
+  {
+    "revision": "d805dfbdcb218aecd43a",
+    "url": "/gobits/static/js/runtime-main.128a2227.js"
+  }
+]);
